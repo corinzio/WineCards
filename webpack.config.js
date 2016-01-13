e72238ea39fa8c4f1c0131e3d3bc08a8319190aa
Luca Briggi <luca.briggi@techedgegroup.com>
2016-01-13 16:35:17 +0100

    added webpack configuration for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 (function () {
     'use strict';
     // Modules
+    var autoprefixer = require('autoprefixer');
     var webpack = require('webpack');
+    var ExtractTextPlugin = require('extract-text-webpack-plugin');    
 
     module.exports = {
         entry: {
@@ -40,8 +42,20 @@
                     // Allow loading html through js
                     test: /\.html$/,
                     loader: 'raw'
+                },
+                {
+                    test: /\.css$/,
+                    // Reference: https://github.com/webpack/extract-text-webpack-plugin
+                    // Extract css files in production builds
+                    //
+                    // Reference: https://github.com/webpack/style-loader
+                    // Use style-loader in development for hot-loading
+                    loader: ExtractTextPlugin.extract('css?sourceMap!postcss')
                 }]
         },
+        postcss: [
+                autoprefixer({browsers: ['last 2 version']})
+        ],
         jshint: {
             camelcase: true,
             emitErrors: false,
@@ -50,7 +64,8 @@
         },
         plugins: [
 		//new webpack.optimize.UglifyJsPlugin(),
-		new webpack.optimize.DedupePlugin()
+		new webpack.optimize.DedupePlugin(),
+        new ExtractTextPlugin('[name].[hash].css')
 	]
     };
 }());
