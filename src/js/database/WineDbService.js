fa7ca22d244a5381f6f0b04ea5138c9eddb554b9
Luca Briggi <luca.briggi@techedgegroup.com>
2016-04-03 21:29:54 +0200

    Started dexie database module

--- src/js/database/WineDbService.js
+++ src/js/database/WineDbService.js
@@ -1,2 +1,27 @@
 import angular from 'angular';
 import Dexie from 'Dexie';
+
+export default class WineDbService{
+  constructor($window, $translate, $q) {
+    this.$q = $q;
+    this.$translate = $translate;
+    this.$window = $window;
+    this.cardsDB = null;
+  }
+
+  createAndOpenDatabase() {
+    this.cardsDB = new Dexie('cards');
+    this.cardsDB.version(1).stores({
+      cards: "++id,wine_name,commercial_name,producer,wine_year,date'
+    });
+
+    //Now open the database
+    this.cardsDB.open();
+  }
+
+  saveWineCard(card){
+    this.cardsDB.cards.add(card);
+  }
+
+}
+WineDbService.$inject = ['$window', '$translate', '$q'];
--- src/js/database/index.js
+++ src/js/database/index.js
@@ -1,6 +1,12 @@
 import angular from 'angular';
+import WineDbService from ./WineDbService.js';
 
 module.exports = (function() {
   'use strict';
-  return angular.module('WineCards.Database', []).name;
+  return angular.module('WineCards.Database', [])
+  .service('WineDbService', WineDbService)
+  .run(['WineDbService',function(db_srv){
+    db_srv.createAndOpenDatabase();
+  }])
+  .name;
 }());
